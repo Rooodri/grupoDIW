5273715adbfa5e696a188cc67c257c7783f1b652
Elena Gata Jiménez <elena.gata@educa.madrid.org>
2022-12-14 11:52:31 +0100

    subida archivos actualizar y subir

---
+++ actualizar.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git pull origin main
+
+echo "---------------------------------------------------------"
+echo "+++++ !Enhorabuena!! has actualizado el repositorio de COMPARTIDO"
+echo "---------------------------------------------------------"
